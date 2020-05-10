20021acbabdc6f587092ec5a5b25d603e2b6e19e
Joyce-Passananti <64074710+Joyce-Passananti@users.noreply.github.com>
2020-05-10 01:42:45 -0700

    Add files via upload
    probably broken

---
+++ controls.js
@@ -0,0 +1,53 @@
+AFRAME.registerComponent("start-interact", {
+    init: function () {
+      var data = this.data;
+      var sceneEl = document.querySelector("a-scene");
+      var entityEl = sceneEl.querySelector("#start");
+  
+      entityEl.addEventListener("mouseenter", function () {
+        entityEl.setAttribute("color", "#636363");
+      });
+  
+      entityEl.addEventListener("mouseleave", function () {
+        entityEl.setAttribute("color", "#212121");
+      });
+  
+      entityEl.addEventListener("click", function () {
+        sceneEl.removeChild(entityEl);
+        startSketch = true;
+        sceneEl.querySelector("#menu0").object3D.visible = "true";
+        sceneEl.querySelector("#menu1").object3D.visible = "true";
+        sceneEl.querySelector("#menu2").object3D.visible = "true";
+      });
+    }
+  });
+  
+  AFRAME.registerComponent("song-interact", {
+    init: function () {
+      var data = this.data;
+      var songEl = this.el;
+      console.log(songEl);
+  
+      songEl.addEventListener("mouseenter", function () {
+        songEl.setAttribute("color", "#636363");
+      });
+  
+      songEl.addEventListener("mouseleave", function () {
+        songEl.setAttribute("color", "#212121");
+      });
+  
+      songEl.addEventListener("click", function () {
+        var sceneEl = document.querySelector("a-scene");
+        var entityEl = sceneEl.querySelector("#menu0");
+        sceneEl.removeChild(entityEl);
+        var entityEl = sceneEl.querySelector("#menu1");
+        sceneEl.removeChild(entityEl);
+        var entityEl = sceneEl.querySelector("#menu2");
+        sceneEl.removeChild(entityEl);
+        // This is your EVENT LISTENER YOU CAN DECIDE TO START THE WAV FILES
+        // FROM HERE
+        console.log(data.track);
+      });
+    }
+  });
+  
